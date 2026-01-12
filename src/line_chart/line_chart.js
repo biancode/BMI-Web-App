cc6b79dd2b4585e8d3c9436573a6c8a3b32172e5
Kay Grewe <kg@iniationware.com>
2026-01-12 14:48:27 +0100

    feat: add doc comments

--- src/line_chart/line_chart.js
+++ src/line_chart/line_chart.js
@@ -1,6 +1,12 @@
 //NOTE(Kay): This will only work when we are using a live server in cases where this is used without a server
 //           it will fail miserably. In these cases we should use a local variable which then can be used inside
 //           plotChart!
+
+/**
+ * 
+ * @param {string} url path to file
+ * @param {*} cb callback to handle the success or error case!
+ */
 function loadJson(url, cb) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
@@ -8,7 +14,7 @@ function loadJson(url, cb) {
 
   xhr.onload = () => {
     if (xhr.status === 200) {
-      cb(null, xhr.response);   // parsed JSON
+      cb(null, xhr.response);
     } else {
       cb(new Error(`HTTP ${xhr.status}`));
     }
@@ -19,6 +25,12 @@ function loadJson(url, cb) {
   xhr.send();
 }
 
+/**
+ * 
+ * @param {string} chartAnchorPoint A id to a canvas element to which the line diagram will be drawn
+ * @param {object} data the bmi data to plot (x and y axis) 
+ * @returns a new Chart.js Plot which contains the plotted data
+ */
 function plotChart(chartAnchorPoint, data) {
   const chartAnchor = document.getElementById(chartAnchorPoint);
   const plotData = data["data"];
@@ -37,8 +49,6 @@ function plotChart(chartAnchorPoint, data) {
 
   return new Chart(chartAnchor, configuration);
 }
-
-//plotChart('bmi-chart');
 
 loadJson('data_mock.json', (err, jsonData) => {
   if (err) {
