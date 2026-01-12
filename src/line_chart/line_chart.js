f6238b1100ab27192912b1fbe86213af706f917f
Kay Grewe <kg@iniationware.com>
2026-01-12 14:24:43 +0100

    refactor: add plotChart function

--- src/line_chart/line_chart.js
+++ src/line_chart/line_chart.js
@@ -1,4 +1,3 @@
-(async function() {
   const data = [
     { month: 'Jan', bmi: 34.5 },
     { month: 'Feb', bmi: 32.5 },
@@ -9,66 +8,23 @@
     { month: 'Jul', bmi: 21.2 },
   ];
 
-  new Chart(
-    document.getElementById('bmi-chart'),
-    {
+function plotChart(chartAnchorPoint) {
+    const chartAnchor = document.getElementById(chartAnchorPoint);
+    //TODO(Kay): Load the data with XMLHttpRequest from a mock file and present it here!
+    const configuration = {
       type: 'line',
       data: {
         labels: data.map(row => row.month),
         datasets: [
           {
-            label: 'BMI over time',
-            data: data.map(row => row.bmi)
+            label: 'Body Mass Index',
+            data: data.map(row => row.bmi),
           }
         ]
       }
-    }
-  );
-})()
+    };
 
+    return new Chart(chartAnchor, configuration);
+  }
 
-//Userstory: Der Nutzer möchte seinen BMI-Wert für den Zeitraum November-Dezember visualisieren,
-//           hierzu wählt er im Dashboard als Start November und Dezember als ende. Als nächstes
-//           drückt er den "aktualisieren" Knopf, woraufhin die Daten vom HTTP Server abgerufen werden.
-//           Danach werden die Daten im Graphen visualisiert.
-
-// Nutzer Einstellungen bezüglich Zeitraum abrufen
-//   HTTP Request vom Server: Daten des nötigen Zeitraums abrufen
-// Graphen konfigurieren
-//  - X als Zeit als Zeitachse mit dem Start- und Enddatum kofigurieren
-//  - Y Die einzelnen Datenpunkte eintragen.
-
-//Funktion retrieveDatapoints(start, end)
-//  try {
-//      daten := requestData("/bmi", {start: 01.11.25, end: 1.12.25})
-//      return daten
-//  } catch (Ex) {
-//      Fehler:
-//       - Verbindung fehlgeschlagen
-//       - Anfrage fehlerhaft
-//       - Serverfehler
-//  }
-//
-
-/*const config = {
-    type: 'line',
-}*/
-
-/*function retrieveDatapoints(start, end) {
-
-}*/
-
-//Funktion plotGraph(start, end)
-//   daten := retrieveDatapoints(start, end)
-//   setupGraph(start, end)
-//   für datenpunkt in daten {
-//      plotData(datenpunkt.datum, datenpunkt.wert)
-//}
-
-
-/*function plotGraph(start, end) {
-    new Chart(ctx, {
-        type: 'line',
-        
-    })
-}*/
+plotChart('bmi-chart');
